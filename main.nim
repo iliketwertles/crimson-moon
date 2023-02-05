960482e73989f9dd77e3d290b725843c5dd93b7d
iliketwertles <67071538+iliketwertles@users.noreply.github.com>
2023-02-05 14:47:00 -0500

    add update check
    checks for updates, promps user to redo bootanimation if one did happen

--- main.nim
+++ main.nim
@@ -23,30 +23,36 @@ proc bootAnimBackup() =
     copyFile("/etc/init/boot-splash.conf", joinPath(backupDir, "boot-splash.conf"))
 
 proc bootAnimSet() =
-    var isRoot = geteuid() == 0
-    if not isRoot:
-        echo "run as root bruh"
-        quit(0)
-    else:
-        let frameInterval = open(joinPath(paramStr(2), "frame-interval"))
-        let configFile = readFile("/etc/init/boot-splash.conf")
-        let newConfig = configFile.replace("25", fmt("<{frameInterval.readLine()} * 1000>"))
-        removeFile("/etc/init/boot-splash.conf")
-        writeFile("/etc/init/boot-splash.conf", newConfig)
-        for kind, file in walkDir("/usr/share/chromeos-assets/images_100_percent"):
-            if "boot" in $file:
-                removeFile($file)
-        for kind, file in walkDir("/usr/share/chromeos-assets/images_200_percent"):
-            if "boot" in $file:
-                removeFile($file)
-        for kind, file in walkDir(paramStr(2)):
-            if "boot" in $file:
-                copyFileToDir($file, "/usr/share/chromeos-assets/images_100_percent")
-                copyFileToDir($file, "/usr/share/chromeos-assets/images_200_percent")
+    let frameInterval = open(joinPath(paramStr(2), "frame-interval"))
+    let configFile = readFile("/etc/init/boot-splash.conf")
+    let newConfig = configFile.replace("25", fmt("<{frameInterval.readLine()} * 1000>"))
+    removeFile("/etc/init/boot-splash.conf")
+    writeFile("/etc/init/boot-splash.conf", newConfig)
+    for kind, file in walkDir("/usr/share/chromeos-assets/images_100_percent"):
+        if "boot" in $file:
+            removeFile($file)
+    for kind, file in walkDir("/usr/share/chromeos-assets/images_200_percent"):
+        if "boot" in $file:
+            removeFile($file)
+    for kind, file in walkDir(paramStr(2)):
+        if "boot" in $file:
+            copyFileToDir($file, "/usr/share/chromeos-assets/images_100_percent")
+            copyFileToDir($file, "/usr/share/chromeos-assets/images_200_percent")
 
 
 
 #main
+var isRoot = geteuid() == 0
+if not isRoot:
+    echo "run as root bruh"
+    quit(0)
+let help = "this will be a help message later :P"
+let update = fileExists("/usr/share/chromeos-assets/Crimson-Check.dont.delete")
+if not update:
+    echo "This is either your first run or an update has happened recently :(, re-apply your boot animation if you set one"
+    writeFile("/usr/share/chromeos-assets/Crimson-Check.dont.delete", "Just putting sum here for funzies")
+else:
+    echo "No updates recently :)"
 let help = "this will be a help message later :P"
 case paramCount()
 of 0:
