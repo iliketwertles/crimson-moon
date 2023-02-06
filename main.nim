f412c133059293a1243883863f305f8bea8c64c0
iliketwertles <67071538+iliketwertles@users.noreply.github.com>
2023-02-05 23:02:44 -0500

    add init function

--- main.nim
+++ main.nim
@@ -5,6 +5,26 @@ import strutils
 import strformat
 
 #functions
+proc crimsonInit() =
+    #checks if grub was tampered with
+    let grubFile = readFile("/boot/efi/boot/grub.cfg")
+    if "cros_debug" in grubFile:
+        echo "Grubs fine :)"
+    else:
+        let fixedGrub = grubFile.replace("noresume", "noresume cros_debug")
+        removeFile("/boot/efi/boot/grub.cfg")
+        writeFile("/boot/efi/boot/grub.cfg", fixedGrub)
+        echo "Grub needed to be fixed but its all good now"
+    #checks if any updates happened
+    let update = fileExists("/usr/share/chromeos-assets/Crimson-Check.dont.delete")
+    if not update:
+        echo "This is either your first run or an update has happened recently :(, re-apply your boot animation if you set one"
+        writeFile("/usr/share/chromeos-assets/Crimson-Check.dont.delete", "Just putting sum here for funzies")
+    else:
+        echo "No updates recently :)"
+    #remounts / as rw for fun :)
+    discard execShellCmd("sudo mount -o remount,rw /")
+
 proc bootAnimBackup() =
     let backupDir = joinPath(getHomeDir(), ".backup") 
     echo "Checking for backup directory..."
@@ -23,37 +43,31 @@ proc bootAnimBackup() =
     copyFile("/etc/init/boot-splash.conf", joinPath(backupDir, "boot-splash.conf"))
 
 proc bootAnimSet() =
-    let frameInterval = open(joinPath(paramStr(2), "frame-interval"))
-    let configFile = readFile("/etc/init/boot-splash.conf")
-    let newConfig = configFile.replace("25", fmt("<{frameInterval.readLine()} * 1000>"))
-    removeFile("/etc/init/boot-splash.conf")
-    writeFile("/etc/init/boot-splash.conf", newConfig)
-    for kind, file in walkDir("/usr/share/chromeos-assets/images_100_percent"):
-        if "boot" in $file:
-            removeFile($file)
-    for kind, file in walkDir("/usr/share/chromeos-assets/images_200_percent"):
-        if "boot" in $file:
-            removeFile($file)
-    for kind, file in walkDir(paramStr(2)):
-        if "boot" in $file:
-            copyFileToDir($file, "/usr/share/chromeos-assets/images_100_percent")
-            copyFileToDir($file, "/usr/share/chromeos-assets/images_200_percent")
-
-
+        let frameInterval = open(joinPath(paramStr(2), "frame-interval"))
+        let configFile = readFile("/etc/init/boot-splash.conf")
+        let newConfig = configFile.replace("25", fmt("<{frameInterval.readLine()} * 1000>"))
+        removeFile("/etc/init/boot-splash.conf")
+        writeFile("/etc/init/boot-splash.conf", newConfig)
+        for kind, file in walkDir("/usr/share/chromeos-assets/images_100_percent"):
+            if "boot" in $file:
+                removeFile($file)
+        for kind, file in walkDir("/usr/share/chromeos-assets/images_200_percent"):
+            if "boot" in $file:
+                removeFile($file)
+        for kind, file in walkDir(paramStr(2)):
+            if "boot" in $file:
+                copyFileToDir($file, "/usr/share/chromeos-assets/images_100_percent")
+                copyFileToDir($file, "/usr/share/chromeos-assets/images_200_percent")
 
 #main
+#start checks
 var isRoot = geteuid() == 0
 if not isRoot:
     echo "run as root bruh"
     quit(0)
 let help = "this will be a help message later :P"
-let update = fileExists("/usr/share/chromeos-assets/Crimson-Check.dont.delete")
-if not update:
-    echo "This is either your first run or an update has happened recently :(, re-apply your boot animation if you set one"
-    writeFile("/usr/share/chromeos-assets/Crimson-Check.dont.delete", "Just putting sum here for funzies")
-else:
-    echo "No updates recently :)"
-let help = "this will be a help message later :P"
+#end checks
+
 case paramCount()
 of 0:
     echo help
@@ -61,6 +75,8 @@ of 1:
     case paramStr(1)
     of "-bb", "--backupbootanim":
         bootAnimBackup()
+    of "-i", "--init":
+        crimsonInit()
     else:
         echo "idk what that means -->" & paramStr(1)
 of 2:
